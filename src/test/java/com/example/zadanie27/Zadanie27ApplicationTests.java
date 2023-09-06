989bad84f76e508857d6d7df6f80d7c3d394ecab
kamilzyczynski <kamil.zyczynski@gmail.com>
2023-09-06 20:37:14 +0200

    Initial commit

---
+++ src/test/java/com/example/zadanie27/Zadanie27ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadanie27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie27ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
