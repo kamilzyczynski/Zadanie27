dd7e91ae21a018ac9ba23c8689ad0814dc083a34
kamilzyczynski <kamil.zyczynski@gmail.com>
2023-09-06 22:52:44 +0200

    Rozwiązanie

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO person (first_name, last_name, birth_date, email, is_signed_up_for_newsletter) VALUES ('Kamil', 'Nowak', '1996-10-10', 'kamil.nowak@gmail.com', false);
+INSERT INTO person (first_name, last_name, birth_date, email, is_signed_up_for_newsletter) VALUES ('Jacek', 'Kowalski', '1987-11-02', 'jkowalski@gmail.com', true);
+INSERT INTO person (first_name, last_name, birth_date, email, is_signed_up_for_newsletter) VALUES ('Anna', 'Kreft', '2023-09-02', 'annak@gmail.com', true);
